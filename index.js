4b64b484b993ab8c1cdd273a01b94ff22d26af17
Andressa Oliveira <andressa.oliveira@unidas.com.br>
2022-07-13 18:52:40 -0300

    teste

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express')
-const path = require('path')
+//const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
- // .use(express.static(path.join(__dirname, 'public')))
- // .set('views', path.join(__dirname, 'views'))
- // .set('view engine', 'ejs')
- // .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  const express = require('express');
+
+ // const express = require('express');
 const app = express();         
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000
@@ -210,8 +205,14 @@ router.patch('/secoesMusica/:id', (req, res) =>{
 
 
 //inicia o servidor
-app.listen(port);
+//app.listen(port);
 console.log('API funcionando!');
+//express()
+  //.use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+ // .set('view engine', 'ejs')
+ // .get('/', (req, res) => res.render('pages/index'))
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
